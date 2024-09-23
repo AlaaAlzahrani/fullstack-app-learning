0d4ea5fc2be31cce2490e64d6a3b97c42f36601e
AlaaAlzahrani <alzahrani.alaaa@gmail.com>
2024-09-24 01:08:37 +0300

    revised commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,30 @@
+import Login from "@/components/Login";
+import Dashboard from "@/components/Dashboard";
+import Main from "@/components/Main"
+
+export const metadata = {
+    title: "Broodl • Dashboard",
+  };
+
+
+export default function DashboardPage() {
+
+    const isAuthenticated = false 
+
+    let children = (
+        <Login />
+    )
+
+    if (isAuthenticated) {
+        children = (
+            <Dashboard />
+        )
+    }
+    return (
+        <Main>
+            {children}
+        </Main>
+    )
+
+
+}
